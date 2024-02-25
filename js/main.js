e6f75d1ba9ff97aae4e947c9fb81260acf2ea151
Mashhoor Khan P <mashhoorkhan325pc@gmail.com>
2024-02-25 12:47:40 +0530

    Updated portfolio

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
-$(window).load(function(){
-	$('#preloader').fadeOut('slow',function(){$(this).remove();});
+$(window).load(function () {
+	$('#preloader').fadeOut(1000, function () {
+		$(this).remove();
+	});
 });
+
 
 
 /******************************************************************************************************************************
 Learn More Page Scroll
 *******************************************************************************************************************************/
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+$(function () {
+	$('a.page-scroll').bind('click', function (event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
 });
 
 /******************************************************************************************************************************
 Menu
-*******************************************************************************************************************************/ 
-(function() {
+*******************************************************************************************************************************/
+(function () {
 
 	var bodyEl = document.body,
 		//content = document.querySelector( '.content-wrap' ),
-		openbtn = document.getElementById( 'open-button' ),
-		closebtn = document.getElementById( 'close-button' ),
+		openbtn = document.getElementById('open-button'),
+		closebtn = document.getElementById('close-button'),
 		isOpen = false;
 
 	function init() {
@@ -32,9 +35,9 @@ Menu
 	}
 
 	function initEvents() {
-		openbtn.addEventListener( 'click', toggleMenu );
-		if( closebtn ) {
-			closebtn.addEventListener( 'click', toggleMenu );
+		openbtn.addEventListener('click', toggleMenu);
+		if (closebtn) {
+			closebtn.addEventListener('click', toggleMenu);
 		}
 
 		/* close the menu element if the target it´s not the menu element or one of its descendants..
@@ -48,11 +51,11 @@ Menu
 	}
 
 	function toggleMenu() {
-		if( isOpen ) {
-			classie.remove( bodyEl, 'show-menu' );
+		if (isOpen) {
+			classie.remove(bodyEl, 'show-menu');
 		}
 		else {
-			classie.add( bodyEl, 'show-menu' );
+			classie.add(bodyEl, 'show-menu');
 		}
 		isOpen = !isOpen;
 	}
